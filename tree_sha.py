5d995d3c72a95284f4219ca20436515588a50e47
Isaac Willemsen <isaac.willemsen@hope.edu>
2023-01-29 17:13:55 -0500

    Finished tree_sha

--- tree_sha.py
+++ tree_sha.py
@@ -1,1 +1,14 @@
-# test
+import sys
+import subprocess
+
+print("Script being run: ", sys.argv[0])
+print("Argument passed: ", sys.argv[1])
+
+cmd = ('git', 'cat-file', '-p', sys.argv[1])
+process = subprocess.run(cmd, capture_output=True, text=True)
+
+output = process.stdout
+
+tree = output.split("\n")[0][5:]
+
+print(tree)
