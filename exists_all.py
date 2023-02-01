7a0d190855aac163dcc744d3d541320fe1a87d83
Isaac Willemsen <isaac.willemsen@hope.edu>
2023-02-01 14:01:26 -0500

    Figuring out bitbucket push issue

---
+++ exists_all.py
@@ -0,0 +1,16 @@
+import sys
+import subprocess
+
+print("Script being run: ", sys.argv[0])
+print("Filename passed: ", sys.argv[1])
+print("")
+
+for commit in sys.argv[1:]:
+    cmd = ('git', 'log', '--name-only', commit)
+    process = subprocess.run(cmd, capture_output=True, text=True)
+    output = process.stdout
+    filesChanged = output.split("\n")[7:]
+    for line in filesChanged:
+        print(line)
+        if line == sys.argv[1]:
+            print("YAY")
