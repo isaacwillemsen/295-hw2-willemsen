ac6b35a8111c3a897beb571dbd6f8b13e6fbcd85
Isaac Willemsen <isaac.willemsen@hope.edu>
2023-03-07 20:29:46 -0500

    Fixed file_contents to only use cat-file

--- file_contents.py
+++ file_contents.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import subprocess
 
@@ -5,20 +6,29 @@ print("Script being run: ", sys.argv[0])
 print("Argument passed: ", sys.argv[1])
 print("")
 
-cmd = ('git', 'log', '-1', sys.argv[1])
-process = subprocess.run(cmd, capture_output=True, text=True)
+getTree = ('git', 'cat-file', '-p', 'main')
 
-output = process.stdout
+process1 = subprocess.run(getTree, capture_output=True, text=True)
+output1 = process1.stdout
 
-author = output.split("\n")[1]
-hash = output.split("\n")[0][6:]
+tree = output1.split("\n")[0][5:]
+author = output1.split("\n")[2][7:]
+print(author)
+print("")
+
+getSha1ForFile = ('git', 'cat-file', '-p', tree)
+process2 = subprocess.run(getSha1ForFile, capture_output=True, text=True)
+output2 = process2.stdout
+
+shas = output2.split("\n")
+for s in shas:
+    if re.search(sys.argv[1], s):
+        sha1 = s[12:52]
+        print("SHA-1 for", sys.argv[1], sha1)
+        print("")
 
 with open(sys.argv[1]) as f:
     lines = f.readlines()
 
-print(author)
-print("")
-print("SHA-1 for", sys.argv[1], hash)
-print("")
 for str in lines:
     print(str)
